d11412a95f0bf391515ac922a42ecea869678347
ChristopherR17 <carrillocrespo17@gmail.com>
2025-01-09 17:33:54 +0100

    .

---
+++ llibres.java
@@ -0,0 +1,11 @@
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.util.Locale;
+
+public class llibres{
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        String filePath = ""
+    }
+}
