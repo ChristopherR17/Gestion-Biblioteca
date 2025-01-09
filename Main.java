9db41d6eea48394af96de43134f43dd2efccdafc
Jordi-debug <jordifrikigamer@gmail.com>
2025-01-09 16:10:11 +0100

    feat: menus Done

--- Main.java
+++ Main.java
@@ -19,6 +19,15 @@ public class Main {
         System.out.println("4. Llistar");
         System.out.println("0. Tornar el menú principal");
     }
+
+    public static void menuListBooks (){
+        System.out.println("Llistar llibres");
+        System.out.println("1. Tots");
+        System.out.println("2. En préstec");
+        System.out.println("3. Per autor");
+        System.out.println("4. Cercar títol");
+        System.out.println("Tornar al menú de llibres");
+    }
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
